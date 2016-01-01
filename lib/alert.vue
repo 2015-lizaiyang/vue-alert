8f006091fccd2cfb17f531e99870216cc1719b6d
baiyaaaaa <bigpet1991@gmail.com>
2016-01-01 23:21:09 +0800

    first commit

--- lib/alert.vue
+++ lib/alert.vue
@@ -1,20 +1,33 @@
 <template>
-  <div class="msgbox" transition="pop-bounce">{{message}}
+  <div class="alertBox" transition="bounce">{{message}}
   </div>
 </template>
 
 <style scope>
-  .msgbox {
+  .alertBox {
     position: absolute;
     top: 50%;
     left: 50%;
-    transform: translate(-50%, -50%);
     padding: 15px 20px;
     background-color: rgba(0,0,0,0.75);
     border-radius: 3px;
     font-size: 13px;
     color: #fff;
+    text-align: center;
     -webkit-user-select: none;
+  }
+  .bounce-transition {
+    transition: all .3s ease;
+    transform: translate(-50%, -50%) scale(1, 1);
+    opacity: 1;
+  }
+  .bounce-enter {
+    transform: translate(-50%, -50%) scale(0.25, 0.25);
+    opacity: 0;
+  }
+  .bounce-leave {
+    transition: all .8s ease;
+    opacity: 0;
   }
 </style>
 
