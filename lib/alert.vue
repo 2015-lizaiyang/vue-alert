4f865f73572faee84c73363fa6f1b617f1aeba95
baiyaaaaa <bigpet1991@gmail.com>
2016-01-01 21:12:17 +0800

    first commit

---
+++ lib/alert.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="msgbox" transition="pop-bounce">{{message}}
+  </div>
+</template>
+
+<style scope>
+  .msgbox {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding: 15px 20px;
+    background-color: rgba(0,0,0,0.75);
+    border-radius: 3px;
+    font-size: 13px;
+    color: #fff;
+    -webkit-user-select: none;
+  }
+</style>
+
+<script type="text/ecmascript-6" lang="babel">
+
+  export default {
+
+    methods: {
+      
+    },
+
+    data() {
+      return {
+        message: ''
+      };
+    }
+  }
+</script>
